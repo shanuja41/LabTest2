afafa27b9cdf8477c1d676c9480878edaa435b90
Shanuja <shanukanthan41@gmail.com>
2025-03-17 10:13:04 +0530

    finished getstarted

---
+++ app/src/main/java/com/example/labtest2/GetstartedActivity.kt
@@ -0,0 +1,22 @@
+package com.example.labtest2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+
+class GetstartedActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.getstarted)
+
+        // **Navigate to login**
+        val getstartedbtn = findViewById<Button>(R.id.getstarted_btn)
+        getstartedbtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/labtest2/loginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.labtest2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class LoginActivity : ComponentActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
