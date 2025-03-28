6e4504927856df7579a3dce7b0f63f79b94d7242
Shanuja <shanukanthan41@gmail.com>
2025-03-28 14:11:31 +0530

    finished home page xml

--- app/src/main/java/com/example/labtest2/HomeActivity.kt
+++ app/src/main/java/com/example/labtest2/HomeActivity.kt
@@ -1,19 +1,24 @@
 package com.example.labtest2
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class HomeActivity : ComponentActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
 
-//      // **Navigate to login**
-//       val getstartedbtn = findViewById<Button>(R.id.getstarted_btn)
-//      getstartedbtn.setOnClickListener {
-//           val intent = Intent(this, SignupActivity::class.java)
-//          startActivity(intent)
-//           finish()
-//       }
+      // **Navigate to login**
+       val burger_btn = findViewById<ImageView>(R.id.burger)
+        burger_btn.setOnClickListener {
+           val intent = Intent(this, SelectfoodActivity::class.java)
+          startActivity(intent)
+           finish()
+       }
     }
 }
